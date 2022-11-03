2c7bf57ad1a92e20e84605ba4865115de2b04061
Claudio Eutizi <claudio.eutizi@mail.polimi.it>
2022-11-03 20:50:21 +0100

    Keyboard più carina

--- keyboard.js
+++ keyboard.js
@@ -5,7 +5,7 @@ const naturalNotes = ["C", "D", "E", "F", "G", "A", "B"];
 const naturalNotesSharps = ["C", "D", "F", "G", "A"];
 const naturalNotesFlats = ["D", "E", "G", "A", "B"];
 
-const range = ["A0", "C6"];
+const range = ["A5", "C7"];
 
 const keyboardApp = {
     setupPiano() {
@@ -19,32 +19,38 @@ const keyboardApp = {
         let whiteKeyPositionX = 0;
 
         allNaturalNotes.forEach((noteName) => {
-            const whiteKeyTextGroup = utils.createSVGElement("g");
+            const whiteKeyAndTextGroup = utils.createSVGElement("g");
             const whiteKey = this.createKey({ className: "white-key", width: whiteKeyWidth, height: pianoHeight });
             const text = utils.createSVGElement("text");
             
             utils.addTextContent(text, noteName);
-            utils.setAttributes(whiteKeyTextGroup, {"width": whiteKeyWidth});
+            utils.setAttributes(whiteKeyAndTextGroup, {
+                "width": whiteKeyWidth,
+                "height": pianoHeight
+            });
             utils.setAttributes(text, {
                 "x": whiteKeyPositionX + whiteKeyWidth / 2,
                 "y": 380,
-                "text-anchor": "middle"
+                "text-anchor": "middle",
             });
             utils.setAttributes(whiteKey, {
                 "x": whiteKeyPositionX,
                 "data-note-name": noteName,
-                "rx": "15",
-                "ry": "15"
+                "fill": "url(#white-key-gradient)",
+                "filter": "url(#white-key-filter)",
+                "rx": "6",
+                "ry": "6"
             });
 
             text.classList.add("white-key-text");
-            whiteKeyTextGroup.appendChild(whiteKey);
-            whiteKeyTextGroup.appendChild(text);
-            SVG.appendChild(whiteKeyTextGroup);
+            whiteKeyAndTextGroup.appendChild(whiteKey);
+            whiteKeyAndTextGroup.appendChild(text);
+            SVG.appendChild(whiteKeyAndTextGroup);
             
             // Increment spacing between keys
             whiteKeyPositionX += whiteKeyWidth;
         });
+
         // Add black keys
         let blackKeyPositionX = 60;
         allNaturalNotes.forEach((naturalNote, index, array) => {
@@ -71,8 +77,10 @@ const keyboardApp = {
                         "x": blackKeyPositionX,
                         "data-sharp-name": `${ naturalSharpNoteName }#${ naturalNote[1]}`,
                         "data-flat-name": `${ naturalFlatNoteName }b${ naturalNote[1]}`,
-                        "rx": "8",
-                        "ry": "8"
+                        "fill": "url(#black-key-gradient)",
+                        "filter": "url(#black-key-filter)",
+                        "rx": "2",
+                        "ry": "2"
                     });
 
                     utils.setAttributes(sharpNameText, {
@@ -109,6 +117,7 @@ const keyboardApp = {
             SVG.appendChild(blackKeyTextGroup);
         });
          // Add main SVG to piano div
+
          piano.appendChild(SVG);
     },
     createOctave(octaveNumber) {
@@ -122,8 +131,9 @@ const keyboardApp = {
         key.classList.add(className, "key");
         utils.setAttributes(key, {
             "width": width,
-            "height": height
+            "height": height,
         });
+
         return key;
     },
     getAllNaturalNotes([firstNote, lastNote]) {
@@ -168,25 +178,80 @@ const keyboardApp = {
             "version": "1.1",
             "xmlns": "http://www.w3.org/2000/svg",
             "xmlns:xlink": "http://www.w3.org/1999/xlink",
+            "xmlns:svgjs": "http://svgjs.dev/svgjs",
             "viewBox": `0 0 ${pianoWidth} ${pianoHeight}`
         });
-
+        let defs = document.createElementNS("http://www.w3.org/2000/svg","defs");
+        defs.innerHTML = 
+                `<radialGradient id="black-key-gradient">
+                    <stop offset="0%" stop-color="hsl(315, 0%, 0%)"></stop>
+                    <stop offset="100%" stop-color="hsl(227, 0%, 0%)"></stop>
+                </radialGradient>
+                <filter id="black-key-filter" x="-0%" y="-0%" width="100%" height="100%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feTurbulence type="fractalNoise" baseFrequency="0.004 0.001" numOctaves="2" seed="158" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence"></feTurbulence>
+                    <feGaussianBlur stdDeviation="19 0" x="0%" y="0%" width="100%" height="100%" in="turbulence" edgeMode="duplicate" result="blur"></feGaussianBlur>
+                    <feBlend mode="color" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" in2="blur" result="blend"></feBlend>
+                    <feColorMatrix type="saturate" values="3" x="0%" y="0%" width="100%" height="100%" in="blend" result="colormatrix"></feColorMatrix>
+                </filter>
+            
+                <linearGradient gradientTransform="rotate(248, 0.5, 0.5)" x1="50%" y1="0%" x2="50%" y2="100%" id="white-key-gradient">
+                    <stop stop-color="hsl(315, 0%, 100%)" stop-opacity="1" offset="0%"></stop>
+                    <stop stop-color="#d0d0d0" stop-opacity="1" offset="100%"></stop>
+                </linearGradient>
+                <filter id="white-key-filter" x="0%" y="0%" width="100%" height="100%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feTurbulence type="fractalNoise" baseFrequency="0.002 0.006" numOctaves="2" seed="2" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence"></feTurbulence>
+                    <feGaussianBlur stdDeviation="100 100" x="0%" y="0%" width="100%" height="100%" in="turbulence" edgeMode="duplicate" result="blur"></feGaussianBlur>
+                    <feBlend mode="color-dodge" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" in2="blur" result="blend"></feBlend>
+                </filter>
+                
+                <linearGradient gradientTransform="rotate(185, 0.5, 0.5)" x1="50%" y1="0%" x2="50%" y2="100%" id="playing-white-key-gradient">
+                <stop stop-color="rgb(189, 189, 189)" stop-opacity="1" offset="0%"></stop>
+                <stop stop-color="hsl(227, 0%, 42%)" stop-opacity="1" offset="100%"></stop></linearGradient>
+                <filter id="playing-white-key-filter" x="0%" y="0%" width="100%" height="100%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feTurbulence type="fractalNoise" baseFrequency="0.001 0.001" numOctaves="1" seed="2" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence"></feTurbulence>
+            <feGaussianBlur stdDeviation="100 100" x="0%" y="0%" width="100%" height="100%" in="turbulence" edgeMode="duplicate" result="blur"></feGaussianBlur>
+            <feBlend mode="color-dodge" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" in2="blur" result="blend"></feBlend>
+            <feColorMatrix type="saturate" values="3" x="0%" y="0%" width="100%" height="100%" in="blend" result="colormatrix"></feColorMatrix>
+          </filter>
+          <radialGradient id="playing-black-key-gradient">
+          <stop offset="0%" stop-color="rgb(51, 51, 51)"></stop>
+          <stop offset="100%" stop-color="hsl(227, 0%, 22%)"></stop>
+        </radialGradient>
+        <filter id="playing-black-key-filter" x="0%" y="0%" width="100%" height="100%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feTurbulence type="fractalNoise" baseFrequency="0.004 0.002" numOctaves="1" seed="2" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence"></feTurbulence>
+        <feGaussianBlur stdDeviation="0 0" x="0%" y="0%" width="100%" height="100%" in="turbulence" edgeMode="duplicate" result="blur"></feGaussianBlur>
+        <feBlend mode="color" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" in2="blur" result="blend"></feBlend>
+        <feColorMatrix type="saturate" values="3" x="0%" y="0%" width="100%" height="100%" in="blend" result="colormatrix"></feColorMatrix>
+            </filter>`;
+      svg.appendChild(defs);
         return svg;
     },
-    displayNotes(notes) {
-        const pianoKeys = document.querySelectorAll(".key");
-        utils.removeClassFromNodeCollection(pianoKeys, "show");
 
-        notes.forEach(noteName => {
+    displayPlayingNotes(e) {
+        const mouseNote = e.target.getAttribute("data-note-name")
+        const pianoKeys = document.querySelectorAll(".key");
+        utils.removeClassFromNodeCollection(pianoKeys, "playing");
             pianoKeys.forEach(key => {
                 const naturalName = key.dataset.noteName;
                 const sharpName = key.dataset.sharpName;
                 const flatName = key.dataset.flatName;
-
-                if (naturalName === noteName || sharpName === noteName || flatName === noteName) {
-                    key.classList.add("show");
+                if(mouseNote == naturalName || mouseNote == sharpName || mouseNote == flatName){
+                    key.classList.add("playing");
                 }
-            });
+        });
+    },
+    displayNoteOnMousePassage(note){
+        object.addEventListener("mouseover", myScript);
+        const pianoKeys = document.querySelectorAll(".key");
+        utils.removeClassFromNodeCollection(pianoKeys, "mouse-highlighted");            
+        pianoKeys.forEach(key => {
+            const naturalName = key.dataset.noteName;
+            const sharpName = key.dataset.sharpName;
+            const flatName = key.dataset.flatName;
+
+            if (naturalName === noteName || sharpName === noteName || flatName === noteName) {
+                key.classList.add("mouse-highlighted");
+            }
         });
     }
 }
@@ -214,3 +279,8 @@ const utils = {
 }
 
 keyboardApp.setupPiano();
+
+const pianoKeys = document.querySelectorAll(".key");
+pianoKeys.forEach(key => {
+    key.addEventListener("click", (e) => {keyboardApp.displayPlayingNotes(e)});
+});
