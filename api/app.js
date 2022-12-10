942a76a95a4721b6eabb1e989ff0f23b2f45ea57
Claudio Eutizi <claudio.eutizi@mail.polimi.it>
2022-12-10 14:07:47 +0100

    Other packages

--- api/app.js
+++ api/app.js
@@ -16,6 +16,7 @@ var cors = require("cors");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require("./routes/testAPI");
+var serialDataRouter = require("./routes/serialData")
 var app = express();
 
 // view engine setup
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
+app.use("/serialData", serialDataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -51,98 +53,3 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-// const serialPort = new SerialPort({path: 'COM9', baudRate: 115200,});
-// const parser = serialPort.pipe(new ReadlineParser({delimiter: '\n'}));
-
-// serialPort.on('open', () => {
-//   console.log('Serial port '+serialPort.path+ ' opened for micro:bit connection!');
-// })
-
-// serialPort.on('close', () => {
-//   console.log('Serial port '+serialPort.path+' closed!');
-// })
-
-// serialPort.on('error', () => {
-//   console.log('Error with the serial port '+serialPort.path+'!');
-// })
-
-// /* ===========================================SERIAL=================================================*/
-// var lightValues = [];
-// var temperatureValues = [];
-// var humidityValues = [];
-
-// parser.on('data', (data) => {
-//   let param = data.substring(0, data.indexOf(':'));
-//   let value = data.substring(data.indexOf(':')+1, data.length);
-//   switch(param){
-//     case "Light":
-//       lightValues.push({
-//         id: uuidv4(),
-//         light: value,
-//         timestamp: moment().format()
-//     })
-//       break;
-//     case "Temperature":
-//       temperatureValues.push({
-//         id: uuidv4(),
-//         temperature: value,
-//         timestamp: moment().format()
-//     })
-//       break;
-//     case "Humidity":
-//       humidityValues.push({
-//         id: uuidv4(),
-//         humidity: value,
-//         timestamp: moment().format()
-//     })
-//   }
-// })
-
-// /* =============================================================UTILS GET===================================================*/
-
-// server.get('/api/light', (req, res) => {
-//   res.json(lightValues)
-// })
-
-// server.get('/api/temperature', (req, res) => {
-//   res.json(temperatureValues)
-// })
-
-// server.get('/api/humidity', (req, res) => {
-//   res.json(humidityValues)})
-
-// server.get('/api/humidity/:id', (request, response) => {
-//     const id = request.params.id;
-//     const hum = humidityValues.find(hum => hum.id === id)
-//     if(hum){
-//       response.json(hum)
-//    } else {
-//        response.status(404).end()
-//    }
-// })
-
-// server.get('/api/temperature/:id', (request, response) => {
-//   const id = request.params.id;
-//   const tem = temperatureValues.find(tem => tem.id === id)
-//   if(tem){
-//     response.json(tem)
-//  } else {
-//      response.status(404).end()
-//  }
-// })
-
-// server.get('/api/light/:id', (request, response) => {
-//   const id = request.params.id;
-//   const lig = temperatureValues.find(lig => lig.id === id)
-//   if(lig){
-//     response.json(lig)
-//  } else {
-//      response.status(404).end()
-//  }
-// })
-// /* ================================================================UTILS DELETE==================================================*/
-// server.delete('/api/humidity/:id', (request, response)=> {
-//   const id = request.params.id
-//   const hum = humidityValues.filter(hum => hum.id !== id)
-//   response.status(204).end()
-// })
