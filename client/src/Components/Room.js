c7b51577bbefde004b168dd861ad80beb087f1bd
Claudio Eutizi <claudio.eutizi@mail.polimi.it>
2023-01-18 09:29:40 +0100

    Room

--- client/src/Components/Room.js
+++ client/src/Components/Room.js
@@ -8,7 +8,7 @@ const Room = () => {
   return (
     <Box flex = {6} p = {2}>
         <h2>ROOM</h2>
-        <WebAudioKnob src={"client\\src\\Components\\images\\Carbon.png"} sprites={60} />;
+        <WebAudioKnob src={""} sprites={60} />;
         <img src={""} alt="Aqua"/>
     </Box> 
   )
