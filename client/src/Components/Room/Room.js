aca73c13faca306da849e088c7ffbeea94a03889
Claudio Eutizi <claudio.eutizi@mail.polimi.it>
2023-02-02 10:09:29 +0100

    Modifiche Room

--- client/src/Components/Room/Room.js
+++ client/src/Components/Room/Room.js
@@ -21,15 +21,6 @@ const Room = ({ onMapClicked, onInstrumentClicked, weatherData, city }) => {
       const sunrise = moment.unix(weatherData.sys.sunrise).tz(weatherData.timezoneString.replace(/__/g, '/'));
       const sunset = moment.unix(weatherData.sys.sunset).tz(weatherData.timezoneString.replace(/__/g, '/'));
 
-      console.log("time: ", time.format());
-      console.log("sunrise:", sunrise.format());
-      console.log("sunset: ", sunset.format());
-
-      console.log("sunset test: ",time.isBetween(sunset.clone().subtract(30, "minutes"), sunset.clone().add(30, 'minutes')));
-      console.log("sunrise test: ",time.isBetween(sunrise.clone().subtract(30, "minutes"), sunrise.clone().add(30, 'minutes')));
-      console.log("day test: ", time.isBetween(sunrise.clone(), sunset.clone()));
-      console.log("night test: ", time.isBetween(sunset.clone().add(30, 'minutes'), sunrise.clone().subtract(30, 'minutes')));
-
       if (time.isBetween(sunset.clone().subtract(30, "minutes"), sunset.clone().add(30, 'minutes'), "minutes", "[]")) {
         setDayMoment("sunset");
         console.log("sunset");
