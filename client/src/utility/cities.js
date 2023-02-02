1abeaba1c4d51fde5da1d53c02a82dd9db7110b4
Claudio Eutizi <claudio.eutizi@mail.polimi.it>
2023-02-02 10:28:35 +0100

    Cairo, NY, Beijing, Paris aggiunte

--- client/src/utility/cities.js
+++ client/src/utility/cities.js
@@ -1,5 +1,27 @@
 const cities = [
     {
+        name: 'London',
+        img: "/images/cities/london.png",
+        //mello
+        instId: 0,
+        id: 'Q84',
+        position: {
+            lat: 51.509865,
+            lon: -0.118092,
+        },
+    },
+    {
+        name: 'New Delhi',
+        img: "/images/cities/new_delhi.png",
+        //sitar
+        instId: 1,
+        id: 'Q987',
+        position: {
+            lat: 28.679079,
+            lon: 77.069710,
+        }
+    },
+    {
         name: 'Madrid',
         //classic guitar
         instId: 2,
@@ -11,13 +33,80 @@ const cities = [
         }
     },
     {
+        name: 'Mexico City',
+        img: "/images/cities/mexico_city.png",
+        //marimba
+        instId: 3,
+        id: 'Q1489',
+        position: {
+            lat: 19.432608,
+            lon: -99.133209,
+        }
+    },
+    {
+        name: 'Cremona',
+        img: "/images/cities/cremona.png",
+        //violin
+        instId: 4,
+        id: 'Q6231',
+        position: {
+            lat: 45.116667,
+            lon: 10.033333,
+        }
+    },
+    {
+        name: 'Paris',
+        img: "/images/cities/paris.png",
+        instId: 5,
+        //harp
+        id: 'Q90',
+        position: {
+            lat: 48.864716,
+            lon: 2.349014,
+        }
+    },
+    {
         name: 'Kinshasa',
         img: "/images/cities/kinshasa.png",
+        instId: 6,
         //kalimba
         id: 'Q3838',
         position: {
             lat: -4.322447,
             lon: 15.307045,
+        }
+    },
+    {
+        name: 'Cairo',
+        img: "/images/cities/cairo.png",
+        instId: 7,
+        //Oud
+        id: 'Q85',
+        position: {
+            lat: 30.0444196,
+            lon: 31.2357116,
+        }
+    },
+    {
+        name: 'Beijing',
+        img: "/images/cities/beijing.png",
+        //Yangqin
+        instId: 8,
+        id: 'Q956',
+        position: {
+            lat: 39.916668,
+            lon: 116.383331,
+        }
+    },
+    {
+        name: 'New York',
+        img: "/images/cities/new_york.png",
+        //brass
+        instId: 9,
+        id: 'Q60',
+        position: {
+            lat: 40.730610,
+            lon: -73.935242,
         }
     },
     {
@@ -28,16 +117,6 @@ const cities = [
         position: {
             lat: -33.918861,
             lon: 18.423300,
-        }
-    },
-    {
-        name: 'Paris',
-        img: "/images/cities/paris.png",
-        //harp
-        id: 'Q90',
-        position: {
-            lat: 48.864716,
-            lon: 2.349014,
         }
     },
     {
@@ -61,17 +140,6 @@ const cities = [
         }
     },
     {
-        name: 'Mexico City',
-        img: "/images/cities/mexico_city.png",
-        //marimba
-        instId: 3,
-        id: 'Q1489',
-        position: {
-            lat: 19.432608,
-            lon: -99.133209,
-        }
-    },
-    {
         name: 'Athens',
         img: "/images/cities/athens.png",
         //buzuki
@@ -92,27 +160,6 @@ const cities = [
         }
     },
     {
-        name: 'Cairo',
-        img: "/images/cities/cairo.png",
-        //Oud
-        id: 'Q85',
-        position: {
-            lat: 30.0444196,
-            lon: 31.2357116,
-        }
-    },
-    {
-        name: 'New Delhi',
-        img: "/images/cities/new_delhi.png",
-        //sitar
-        instId: 1,
-        id: 'Q987',
-        position: {
-            lat: 28.679079,
-            lon: 77.069710,
-        }
-    },
-    {
         name: 'Tokyo',
         img: "/images/cities/tokyo.png",
         //koto
@@ -121,50 +168,6 @@ const cities = [
             lat: 35.672855,
             lon: 139.817413,
         }
-    },
-    {
-        name: 'Beijing',
-        img: "/images/cities/beijing.png",
-        //Yangqin
-        id: 'Q956',
-        position: {
-            lat: 39.916668,
-            lon: 116.383331,
-        }
-    },
-    {
-        name: 'Cremona',
-        img: "/images/cities/cremona.png",
-        //violin
-        instId: 4,
-        id: 'Q6231',
-        position: {
-            lat: 45.116667,
-            lon: 10.033333,
-        }
-    },
-
-    {
-        name: 'New York',
-        img: "/images/cities/new_york.png",
-        //brass
-        id: 'Q60',
-        position: {
-            lat: 40.730610,
-            lon: -73.935242,
-        }
-    },
-
-    {
-        name: 'London',
-        img: "/images/cities/london.png",
-        //mello
-        instId: 0,
-        id: 'Q84',
-        position: {
-            lat: 51.509865,
-            lon: -0.118092,
-        },
     },
     {
         name: 'Sydney',
