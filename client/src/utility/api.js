0e8bce7821c92c8ee869a698953befaa2eeb4738
Claudio Eutizi <claudio.eutizi@mail.polimi.it>
2023-02-11 11:24:44 +0100

    API

--- client/src/utility/api.js
+++ client/src/utility/api.js
@@ -1,14 +1,14 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '5f0de96521msh612d083876ac5f9p1a82afjsnb3321382bc51',
+		'X-RapidAPI-Key': '[YOUR_RAPID_API_HERE]',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
-export const WEATHER_API_KEY = 'dc36bb48e82a90d6e689af6a87acdb60'
+export const WEATHER_API_KEY = '[YOUR_OPENWEATHER_API_HERE]'
 export const WEATHER_ICON_URL = 'https://openweathermap.org/img/w'
 export const MAP_TILER = {
 	maptiler: {
